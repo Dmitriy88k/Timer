0063a231c8dc02fc651c9b2f75c3373fd77c31bf
Dmitriy Khegay <dmitriy88k@gmail.com>
2023-07-13 21:46:45 -0400

    started working on timer in js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const pause = document.getElementById('pause_button');
+const play = document.getElementById('play_button');
+const stop = document.getElementById('stop_button');
+
+const hourField = document.getElementById('hour');
+const minutesField = document.getElementById('minutes');
+const secondsField = document.getElementById('seconds');
+
+play.addEventListener('click', function(){
+  console.log("hello");
+})
